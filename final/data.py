06f4b18cbfab9fd4ac28ae961880425aa0b916da
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2015-12-03 11:29:39 -0500

    Add util funciton and submission.

--- final/data.py
+++ final/data.py
@@ -1,7 +1,35 @@
 import numpy as np
+from nltk.stem.porter import PorterStemmer
+
+stemmer = PorterStemmer()
+
+with open('./data/attributes_list.txt') as f:
+    attr_texts = [attr_text[1:-2].replace('/', ' ').split() for attr_text in f]
+
+def load_attributes(file_name):
+    with open(file_name) as f:
+        attrs = [map(int, row.split()[1].split(',')) for row in f]
+    return np.array(attrs, dtype=int)
+
+def load_attribute_texts(attrs):
+    texts = []
+    for attr in attrs:
+        text = []
+        for i in range(len(attr)):
+            if attr[i] == 1:
+                text.extend(attr_texts[i])
+        texts.append(set(map(stemmer.stem, text)))
+    return texts
+
 
 train_feat_alexnet = np.load('./data/alexnet_feat_train.npy')
 train_feat_sift = np.load('./data/SIFTBoW_train.npy')
+train_feat_attr = load_attributes('./data/attributes_train.txt')
+train_raw_attr = load_attribute_texts(train_feat_attr)
+
+test_feat_alexnet = np.load('./data/alexnet_feat_test.npy')
+test_feat_sift = np.load('./data/SIFTBoW_test.npy')
+
 
 with open('./data/train.txt') as data:
     labels = list(set([row.split()[1] for row in data]))
@@ -10,6 +38,11 @@ with open('./data/train.txt') as data:
 with open('./data/train.txt') as data:    
     train_target = np.array([labels_dict[row.split()[1]] for row in data]).astype(int)
 
+
 ten_k_feat_alexnet = np.load('./data/alexnet_feat_10k.npy')
 ten_k_feat_sift = np.load('./data/SIFTBoW_10k.npy')
+
+with open('./data/attributes_list.txt') as data:
+    attributes = [row for row in data]
+
 
---
+++ final/util.py
@@ -0,0 +1,19 @@
+import os
+import csv
+import data
+
+def output_submission(prediction):
+    latest = 0
+    for f in os.listdir('submission'):
+        n, ext = os.path.splitext(f)
+        if ext == '.csv':
+            latest = max(int(n.replace('0', '')), latest)
+
+    new_file_path = os.path.join('submission', '%03d.csv' % (latest + 1))
+    print("Generating new file for submision: %s..." % (new_file_path))
+    
+    with open(new_file_path, 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(['ID', 'Category'])
+        for i in range(len(prediction)):
+                 writer.writerow(['%04d.jpg' % (i), data.labels[prediction[i]]])
